d646a8c3b2f11049ad812ecca38def76a7c48b8a
NicolasH-MDS <nicolas.hubert@my-digital-school.org>
2025-10-09 14:19:27 +0200

    erreur si slug === unknown

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,14 +1,22 @@
+import { notFound } from "next/navigation";
+
 type Props = { params: { slug: string } };
 
 // Fonction pour générer les métadonnées dynamiquement
 export async function generateMetadata({ params }: Props) {
+  if (params.slug === "unknown") {
+    notFound();
+  }
   return {
-    title: `Article : ${params.slug} — Eco Shop`,
-    description: `Découvrez l'article de blog "${params.slug}" sur Eco Shop.`,
+    title: `Article : ${params.slug} — Next Shop`,
+    description: `Découvrez l'article de blog "${params.slug}" sur Next Shop.`,
   };
 }
 
 export default function BlogPostPage({ params }: Props) {
+  if (params.slug === "unknown") {
+    notFound();
+  }
   return (
     <main>
       <h1>Article de blog : {params.slug}</h1>
