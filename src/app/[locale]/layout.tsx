4209d558a00539cbf9aeb5ca6a4964308d41bfc0
Fl4m3urDev <VoxFlameur22@gmail.com>
2025-10-09 11:49:52 +0200

    feat. update project next.js + add more folders and files...

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,26 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { getTranslations } from 'next-intl/server';
+import "@/styles/globals.css";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+export default async function LocaleLayout({
+  children, params: { locale }
+}: { children: React.ReactNode; params: { locale: string } }) {
+  let messages;
+  try { messages = (await import(`@/messages/${locale}.json`)).default; }
+  catch { notFound(); }
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Header />
+          <main className="container">{children}</main>
+          <Footer />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
